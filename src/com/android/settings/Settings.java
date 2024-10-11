ae5cafa46b93ffd275a6bf7ac556655cf1d3430d
Joe Maples <joe@frap129.org>
2024-10-11 17:28:00 +0800

    Settings: Integrate Personalizations into Settings
    Change-Id: Id86228181a2ae84008a177d42d1fc94317eb2dc8
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -522,4 +522,10 @@ public class Settings extends SettingsActivity {
     public static class HearingDevicesActivity extends SettingsActivity { /* empty */ }
     public static class HearingDevicesPairingActivity extends SettingsActivity { /* empty */ }
     public static class ContactsStorageSettingsActivity extends SettingsActivity { /* empty */ }
+
+    /** Personalizations Activities **/
+    public static class PersonalizationsActivity extends SettingsActivity {}
+    public static class PersonalizationsThemesActivity extends SettingsActivity {}
+    public static class PersonalizationsLockscreenActivity extends SettingsActivity {}
+    public static class PersonalizationsToolboxActivity extends SettingsActivity {}
 }
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -211,6 +211,9 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.rising.settings.PersonalizationsFragment;
+import com.rising.settings.fragments.*;
+
 public class SettingsGateway {
 
     /**
@@ -409,7 +412,11 @@ public class SettingsGateway {
             HearingDevicePairingFragment.class.getName(),
             ZenModesListFragment.class.getName(),
             ZenModeFragment.class.getName(),
-            ContactsStorageSettings.class.getName()
+            ContactsStorageSettings.class.getName(),
+            PersonalizationsFragment.class.getName(),
+            LockScreen.class.getName(),
+            Themes.class.getName(),
+            Toolbox.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -456,5 +463,9 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.PersonalizationsActivity.class.getName(),
+            Settings.PersonalizationsLockscreenActivity.class.getName(),
+            Settings.PersonalizationsToolboxActivity.class.getName(),
+            Settings.PersonalizationsThemesActivity.class.getName()
     };
 }
