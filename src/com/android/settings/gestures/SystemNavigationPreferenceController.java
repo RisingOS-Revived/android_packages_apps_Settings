f435f1d0db79404be932c1f00f61748beb27767e
minaripenguin <minaripenguin@users.noreply.github.com>
2026-07-30 04:59:15 +0530

    Settings: Always make gesture navigation settings available in A17
    needed for Quickstep
    
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>
    Change-Id: Ia07e2a0ca5d4aa5c08803c0de756bdd6caa0cfc8
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- src/com/android/settings/gestures/SystemNavigationPreferenceController.java
+++ src/com/android/settings/gestures/SystemNavigationPreferenceController.java
@@ -72,26 +72,7 @@ public class SystemNavigationPreferenceController extends BasePreferenceControll
 
         // Skip if the swipe up settings are not available
         // or if on-screen navbar is disabled (for devices with hardware keys)
-        if (!configEnabled || !hasNavigationBar) {
-            return false;
-        }
-
-        // Skip if the recents component is not defined
-        final ComponentName recentsComponentName = ComponentName.unflattenFromString(
-                context.getString(com.android.internal.R.string.config_recentsComponentName));
-        if (recentsComponentName == null) {
-            return false;
-        }
-
-        // Skip if the overview proxy service exists
-        final Intent quickStepIntent = new Intent(ACTION_QUICKSTEP)
-                .setPackage(recentsComponentName.getPackageName());
-        if (context.getPackageManager().resolveService(quickStepIntent,
-                PackageManager.MATCH_SYSTEM_ONLY) == null) {
-            return false;
-        }
-
-        return true;
+        return configEnabled && hasNavigationBar;
     }
 
     static boolean isOverlayPackageAvailable(Context context, String overlayPackage) {
